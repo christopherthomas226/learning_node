d91904c54c85cd6825dc7a45a255c36764cc709c
christopherthomas226 <cat044@ucsd.edu>
2019-07-02 19:27:34 -0700

    cross fingers

--- schema.js
+++ schema.js
@@ -1,4 +1,5 @@
 exports.typeDefs = `
+
 type Recipe {
     _id: ID
     name: String!
--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const mongooose = require('mongoose')
 
 const bodyParser = require('body-parser')
 
-const path = require('path')
-
 const cors = require('cors')
 
 const jwt = require('jsonwebtoken')
+
+const path = require('path')
 
 require('dotenv').config({path:'variables.env'})
 
@@ -55,7 +55,7 @@ app.use(async (req,res,next) => {
     next()
 })
 // Create GraphiQl application
-//app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql'}))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql'}))
 
 // Connect schema with GraphQL
 app.use(
@@ -71,15 +71,19 @@ app.use(
     }))
 )
 
-if(process.env.NODE_ENV === 'production'){
+/*if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
 
-    app.get('*', (req,res) =>{
+    app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+
     })
 }
+*/
+
+
 const PORT = process.env.PORT || 4444
 
 app.listen(PORT, () => {
-    console.log(`Server listening on PORT ${PORT}`)
+    //console.log(`Server listening on PORT ${PORT}`)
 })
